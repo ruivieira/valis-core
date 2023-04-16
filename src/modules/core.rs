c32f0e9da0e7c43b8c3503a4eddd506825ecd3b1
Rui Vieira <ruidevieira@googlemail.com>
2023-04-16 21:15:49 +0100

    docs: correct core docs

--- src/modules/core.rs
+++ src/modules/core.rs
@@ -6,13 +6,13 @@ use std::env;
 use std::path::PathBuf;
 use globmatch::Matcher;
 
+/// Run a command string and outputs realtime output to stdout.
+///
+/// # Arguments
+///
+/// * `command` - A string slice that holds the command to be executed
+///
 pub fn run(command: &str) {
-    /// Run a command string and outputs realtime output to stdout.
-    ///
-    /// # Arguments
-    ///
-    /// * `command` - A string slice that holds the command to be executed
-    ///
     let mut tokens = command.split(" ").collect::<Vec<&str>>();
     let location = tokens[0];
     tokens.remove(0);
@@ -31,11 +31,12 @@ pub fn run(command: &str) {
     }
 }
 
+/// Return the OS name on which we are running.
 pub fn get_os() -> String {
-    /// Return the OS name on which we are running.
     return env::consts::OS.to_owned();
 }
 
+/// Return a list of `Matcher` objects that match the given pattern.
 pub fn get_files<'a>(root: PathBuf, pattern: &'a &str) -> Result<Matcher<'a, PathBuf>, String> {
     return globmatch::Builder::new(&pattern).build(root.to_str().unwrap().to_owned());
 }

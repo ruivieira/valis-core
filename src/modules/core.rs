8f245bdda00655033648a6448b5046b02e66d5b3
Rui Vieira <ruidevieira@googlemail.com>
2023-04-16 19:07:16 +0100

    feat: Add get_os to core

--- src/modules/core.rs
+++ src/modules/core.rs
@@ -2,8 +2,16 @@ use std::process::Command;
 use std::process::Stdio;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::env;
+
 
 pub fn run(command: &str) {
+    /// Run a command string and outputs realtime output to stdout.
+    ///
+    /// # Arguments
+    ///
+    /// * `command` - A string slice that holds the command to be executed
+    ///
     let mut tokens = command.split(" ").collect::<Vec<&str>>();
     let location = tokens[0];
     tokens.remove(0);
@@ -20,4 +28,9 @@ pub fn run(command: &str) {
     for line in lines {
         println!("{}", line.unwrap());
     }
+}
+
+pub fn get_os() -> String {
+    /// Return the OS name on which we are running.
+    return env::consts::OS.to_owned();
 }

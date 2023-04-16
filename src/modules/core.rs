80ccaaa395bb1b2b017585abc48fd70f54c8b5a1
Rui Vieira <ruidevieira@googlemail.com>
2023-04-16 21:06:16 +0100

    feat: add file getter with glob

--- src/modules/core.rs
+++ src/modules/core.rs
@@ -3,7 +3,8 @@ use std::process::Stdio;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::env;
-
+use std::path::PathBuf;
+use globmatch::Matcher;
 
 pub fn run(command: &str) {
     /// Run a command string and outputs realtime output to stdout.
@@ -33,4 +34,8 @@ pub fn run(command: &str) {
 pub fn get_os() -> String {
     /// Return the OS name on which we are running.
     return env::consts::OS.to_owned();
+}
+
+pub fn get_files<'a>(root: PathBuf, pattern: &'a &str) -> Result<Matcher<'a, PathBuf>, String> {
+    return globmatch::Builder::new(&pattern).build(root.to_str().unwrap().to_owned());
 }

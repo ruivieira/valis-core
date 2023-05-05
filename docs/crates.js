9df882ae0bf733202b277922e39a9a96f9a315af
Rui Vieira <ruidevieira@googlemail.com>
2023-05-05 23:46:14 +0100

    docs: Add crate docs

---
+++ docs/crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["valis_core"];
---
+++ docs/source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"valis_core":["",[["modules",[["k8s",[],["kind.rs","mod.rs"]],["projects",[],["git.rs","mod.rs","venv.rs"]],["script",[],["engine.rs","mod.rs"]],["software",[],["definitions.rs","doctor.rs","mod.rs"]]],["core.rs","log.rs","mod.rs"]]],["lib.rs"]]\
+}');
+createSourceSidebar();

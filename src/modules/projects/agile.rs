d05c836c630cd72acb463e35d4a76275d9f86feb
Rui Vieira <ruidevieira@googlemail.com>
2023-05-06 22:08:06 +0100

    feat: Add REPL

--- src/modules/projects/agile.rs
+++ src/modules/projects/agile.rs
@@ -1,60 +1,60 @@
-use serde::{Deserialize, Serialize};
-use sqlx::{Pool, Sqlite};
-use sqlx::sqlite::SqlitePoolOptions;
-
-use chrono::{DateTime, Utc};
-use uuid::Uuid;
-
-#[derive(Serialize, Deserialize)]
-struct Project {
-    id: Uuid,
-    name: String,
-    description: String,
-    created_at: DateTime<Utc>,
-    updated_at: DateTime<Utc>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Sprint {
-    id: Uuid,
-    project_id: Uuid,
-    name: String,
-    start_date: DateTime<Utc>,
-    end_date: DateTime<Utc>,
-    created_at: DateTime<Utc>,
-    updated_at: DateTime<Utc>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Task {
-    id: Uuid,
-    sprint_id: Option<Uuid>,
-    project_id: Uuid,
-    title: String,
-    description: Option<String>,
-    status: String,
-    priority: String,
-    created_at: DateTime<Utc>,
-    updated_at: DateTime<Utc>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct TaskComment {
-    id: Uuid,
-    task_id: Uuid,
-    content: String,
-    created_at: DateTime<Utc>,
-    updated_at: DateTime<Utc>,
-}
-
-async fn connect() -> Result<Pool<Sqlite>, Box<dyn std::error::Error>> {
-    let database_url = "sqlite:./my_database.sqlite3";
-    let pool = SqlitePoolOptions::new()
-        .max_connections(5)
-        .connect(database_url)
-        .await?;
-
-    // Use the pool to execute queries and interact with the database here
-
-    Ok(pool)
-}
+// use serde::{Deserialize, Serialize};
+// use sqlx::{Pool, Sqlite};
+// use sqlx::sqlite::SqlitePoolOptions;
+//
+// use chrono::{DateTime, Utc};
+// use uuid::Uuid;
+//
+// #[derive(Serialize, Deserialize)]
+// struct Project {
+//     id: Uuid,
+//     name: String,
+//     description: String,
+//     created_at: DateTime<Utc>,
+//     updated_at: DateTime<Utc>,
+// }
+//
+// #[derive(Serialize, Deserialize)]
+// struct Sprint {
+//     id: Uuid,
+//     project_id: Uuid,
+//     name: String,
+//     start_date: DateTime<Utc>,
+//     end_date: DateTime<Utc>,
+//     created_at: DateTime<Utc>,
+//     updated_at: DateTime<Utc>,
+// }
+//
+// #[derive(Serialize, Deserialize)]
+// struct Task {
+//     id: Uuid,
+//     sprint_id: Option<Uuid>,
+//     project_id: Uuid,
+//     title: String,
+//     description: Option<String>,
+//     status: String,
+//     priority: String,
+//     created_at: DateTime<Utc>,
+//     updated_at: DateTime<Utc>,
+// }
+//
+// #[derive(Serialize, Deserialize)]
+// struct TaskComment {
+//     id: Uuid,
+//     task_id: Uuid,
+//     content: String,
+//     created_at: DateTime<Utc>,
+//     updated_at: DateTime<Utc>,
+// }
+//
+// async fn connect() -> Result<Pool<Sqlite>, Box<dyn std::error::Error>> {
+//     let database_url = "sqlite:./my_database.sqlite3";
+//     let pool = SqlitePoolOptions::new()
+//         .max_connections(5)
+//         .connect(database_url)
+//         .await?;
+//
+//     // Use the pool to execute queries and interact with the database here
+//
+//     Ok(pool)
+// }
---
+++ src/modules/script/repl.rs
@@ -0,0 +1,118 @@
+use std::borrow::Cow::{self, Borrowed, Owned};
+
+use rustyline::{CompletionType, Config, EditMode, Editor, Helper};
+use rustyline::completion::{Candidate, Completer};
+use rustyline::Context;
+use rustyline::highlight::Highlighter;
+use rustyline::hint::Hinter;
+use rustyline::validate::Validator;
+
+struct CustomCompleter {
+    commands: Vec<&'static str>,
+}
+
+impl Completer for CustomCompleter {
+    type Candidate = SimpleCandidate;
+
+    fn complete(
+        &self,
+        line: &str,
+        pos: usize,
+        _ctx: &Context<'_>,
+    ) -> Result<(usize, Vec<SimpleCandidate>), rustyline::error::ReadlineError> {
+        let mut candidates = Vec::new();
+        for command in &self.commands {
+            if command.starts_with(line) {
+                candidates.push(SimpleCandidate {
+                    display: command.to_string(),
+                    replacement: command[pos..].to_string(),
+                });
+            }
+        }
+
+        Ok((0, candidates))
+    }
+}
+
+#[derive(Debug, Clone)]
+struct SimpleCandidate {
+    display: String,
+    replacement: String,
+}
+
+impl Candidate for SimpleCandidate {
+    fn display(&self) -> &str {
+        &self.display
+    }
+
+    fn replacement(&self) -> &str {
+        &self.replacement
+    }
+}
+
+struct CustomHelper {
+    completer: CustomCompleter,
+}
+
+impl Helper for CustomHelper {}
+
+impl Completer for CustomHelper {
+    type Candidate = SimpleCandidate;
+
+    fn complete(
+        &self,
+        line: &str,
+        pos: usize,
+        ctx: &Context<'_>,
+    ) -> Result<(usize, Vec<SimpleCandidate>), rustyline::error::ReadlineError> {
+        self.completer.complete(line, pos, ctx)
+    }
+}
+
+impl Hinter for CustomHelper {
+    type Hint = String;
+
+    fn hint(&self, _line: &str, _pos: usize, _ctx: &Context<'_>) -> Option<String> {
+        None
+    }
+}
+
+impl Highlighter for CustomHelper {
+    fn highlight<'l>(&self, line: &'l str, _pos: usize) -> Cow<'l, str> {
+        Borrowed(line)
+    }
+
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        Borrowed(hint)
+    }
+}
+
+impl Validator for CustomHelper {}
+
+pub fn repl() {
+    let config = Config::builder()
+        .edit_mode(EditMode::Emacs)
+        .completion_type(CompletionType::List)
+        .build();
+
+    let commands = vec!["list", "add", "remove", "update", "help", "quit"];
+    let completer = CustomCompleter { commands };
+
+    let helper = CustomHelper { completer };
+    let mut editor = Editor::with_config(config);
+    editor.set_helper(Some(helper));
+
+    loop {
+        let readline = editor.readline(">> ");
+        match readline {
+            Ok(line) => {
+                editor.add_history_entry(line.as_str());
+                println!("You entered: {:?}", line);
+                if line.trim() == "quit" {
+                    break;
+                }
+            }
+            Err(_) => break,
+        }
+    }
+}
